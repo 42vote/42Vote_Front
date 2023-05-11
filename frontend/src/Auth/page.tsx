f45000ff10b3c00bf7fca416d6f1bbd2b3e1512f
parkjoungwan <whddhs6645@gmail.com>
2023-05-11 13:46:07 +0900

    [ADD] AuthPage loading spinner 추가

--- frontend/src/Auth/page.tsx
+++ frontend/src/Auth/page.tsx
@@ -1,24 +1,24 @@
 import { useNavigate } from "react-router-dom";
 import { genToken } from "./util/genToken";
 import { useEffect } from "react";
-import Login from "../Login/login";
 import Cookies from "js-cookie";
+import LoadingModal from "./components/LoadingModal";
 
 interface AuthProps {
-  locationSearch: string,
+  locationSearch: string;
 }
 
-const Auth = ({locationSearch}:AuthProps) => {
+const Auth = ({ locationSearch }: AuthProps) => {
   const query = new URLSearchParams(locationSearch);
   const code = query.get("code");
-  const tokenExist = Cookies.get('token') !== undefined ? true : false;
+  const tokenExist = Cookies.get("token") !== undefined ? true : false;
   const navi = useNavigate();
 
   useEffect(() => {
-    if (code && !tokenExist) genToken(code).then(() => navi("/main"));
+    if (code && !tokenExist) genToken(code).then(() => console.log(""));
   }, [code, tokenExist, navi]);
 
-  return <Login LoginText="Wait a second..." />;
+  return <LoadingModal />;
 };
 
 export default Auth;
