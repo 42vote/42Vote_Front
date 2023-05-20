ae30cdc0a53eb2699821cace1b1f7a24f9467014
parkjoungwan <whddhs6645@gmail.com>
2023-05-20 16:14:37 +0900

    [ADD]Component 정리

--- frontend/src/Main/components/Card/Card.tsx
+++ frontend/src/Main/components/Card/Card.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { CardProps } from "../../types";
 import styled from "styled-components";
-import Test from "../../../Test/test";
 
 const ImgContainer = styled.div<{ imgURL: string }>`
   background-image: url(${(prop) => prop.imgURL});
@@ -30,8 +29,8 @@ const Card: React.FC<CardProps> = ({
     <>
       <div onClick={handleCardClick} className="card">
         <div className="product-card">
-          <ImgContainer imgURL={image}>
-            <img className="product-card__image" src={image} alt={title} />
+          <ImgContainer imgURL={image ? image : "https://play-lh.googleusercontent.com/aFWiT2lTa9CYBpyPjfgfNHd0r5puwKRGj2rHpdPTNrz2N9LXgN_MbLjePd1OTc0E8Rl1"}>
+            <img className="product-card__image" src={image ? image : "https://play-lh.googleusercontent.com/aFWiT2lTa9CYBpyPjfgfNHd0r5puwKRGj2rHpdPTNrz2N9LXgN_MbLjePd1OTc0E8Rl1"} alt={title} />
           </ImgContainer>
           <h2 className="product-card__title">{title}</h2>
           <div className="product-progress">
--- frontend/src/Main/styles/styleComponents.ts
+++ frontend/src/Main/styles/styleComponents.ts
@@ -115,3 +115,47 @@ export const SkeletonTag = styled.div`
     }
   }
 `;
+
+export const ImgContainer = styled.div<{ imgURL: string; mainColor: string }>`
+  background-image: url(${(prop) => prop.imgURL});
+  background-color: ${(prop) => prop.mainColor};
+  background-size: cover;
+  transition: transform 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
+  height: 7.5rem;
+`;
+
+export const Cards = styled.div<{ mainColor: string }>`
+  background-color: ${(prop) => prop.mainColor};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 8rem;
+  height: 9.375rem;
+  margin: 1rem;
+  padding: 1rem;
+`;
+
+export const ProductCard = styled.div<{ mainColor: string }>`
+  background-color: ${(prop) => prop.mainColor};
+  margin: 0.625rem;
+  padding: 1.25rem;
+  border-radius: 0.625rem;
+  height: 11.25rem;
+  transition: transform 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
+  box-shadow: 3px 3px 0.625rem rgba(0, 0, 0, 0.3);
+  cursor: pointer;
+`;
+
+export const ProductCardTitle = styled.h2<{ textColor: string }>`
+  margin-top: 0.3125rem;
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: ${(prop) => prop.textColor};
+  transition: color 0.5s ease-in-out;
+  white-space: nowrap;
+  max-width: 7.5rem;
+  margin-bottom: 0.3125rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
