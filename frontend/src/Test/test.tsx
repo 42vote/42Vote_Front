26d8189104185a2ba1b2e4345d90101f5bfdbd89
parkjoungwan <whddhs6645@gmail.com>
2023-05-22 13:56:41 +0900

    [FEAT] color 추가했다가 다시 주석처리

--- frontend/src/Test/test.tsx
+++ frontend/src/Test/test.tsx
@@ -1,61 +1,7 @@
 import { useColor } from "color-thief-react";
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
-
-function getTextColorByBackgroundColor(hexColor: string) {
-  const c = hexColor.substring(1); // 색상 앞의 # 제거
-  const rgb = parseInt(c, 16); // rrggbb를 10진수로 변환
-  const r = (rgb >> 16) & 0xff; // red 추출
-  const g = (rgb >> 8) & 0xff; // green 추출
-  const b = (rgb >> 0) & 0xff; // blue 추출
-  const luma = 0.2126 * r + 0.7152 * g + 0.0722 * b; // per ITU-R BT.709
-  // 색상 선택
-  return luma < 127.5 ? "white" : "black"; // 글자색이
-}
-
-const ImgContainer = styled.div<{ imgURL: string; mainColor: string }>`
-  background-image: url(${(prop) => prop.imgURL});
-  background-color: ${(prop) => prop.mainColor};
-  background-size: cover;
-  transition: transform 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
-  height: 7.5rem;
-`;
-
-const Cards = styled.div<{ mainColor: string }>`
-  background-color: ${(prop) => prop.mainColor};
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 8rem;
-  height: 9.375rem;
-  margin: 1rem;
-  padding: 1rem;
-`;
-
-const ProductCard = styled.div<{ mainColor: string }>`
-  background-color: ${(prop) => prop.mainColor};
-  margin: 0.625rem;
-  padding: 1.25rem;
-  border-radius: 0.625rem;
-  height: 11.25rem;
-  transition: transform 0.2s ease-in-out, box-shadow 0.2s ease-in-out;
-  box-shadow: 3px 3px 0.625rem rgba(0, 0, 0, 0.3);
-  cursor: pointer;
-`;
-
-const ProductCardTitle = styled.h2<{ textColor: string }>`
-  margin-top: 0.3125rem;
-  font-size: 1.2rem;
-  font-weight: bold;
-  color: ${(prop) => prop.textColor};
-  transition: color 0.5s ease-in-out;
-  white-space: nowrap;
-  max-width: 7.5rem;
-  margin-bottom: 0.3125rem;
-  overflow: hidden;
-  text-overflow: ellipsis;
-`;
+import { getTextColorByBackgroundColor } from "../Lib/getColors";
+import { ImgContainer, Cards, ProductCard, ProductCardTitle } from "../Main/styles/styleComponents";
 
 const Test = () => {
   const imgSrc =
