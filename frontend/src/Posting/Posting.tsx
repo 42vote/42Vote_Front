7a6476fbc83cc2b50839ca300b26ed69bac99060
zoovely <yon041128@naver.com>
2023-05-24 16:44:02 +0900

    [FIX] resolve #15 #17

--- frontend/src/Posting/Posting.tsx
+++ frontend/src/Posting/Posting.tsx
@@ -6,17 +6,11 @@ import { customAxios } from '../Lib/customAxios';
 import { categoryRes } from "../Main/types";
 import Swal from 'sweetalert2';
 
-
 function Posting() {
     const isDesktop = useMediaQuery({query: '(min-width: 769px)'});
-    const isMobile = useMediaQuery({query: '(max-width: 768px)'});
     const [categoryList, setCategoryList] = useState(Array<categoryRes>);
     const [title, setTitle] = useState('New Post Title');
-
-    const handleTitle = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    const target: HTMLInputElement = event.target as HTMLInputElement;
-        setTitle(target.value); //왜 포커스가 풀리지?
-    }
+    const [descriptLength, setDescriptLength] = useState(0);
 
     useEffect(() => {
         customAxios().get('/category').then((res) => {
@@ -69,7 +63,7 @@ function Posting() {
         }
     }
 
-    const getCheckedCategory = async () => {
+    const getCheckedCategory = () => {
         const categorys = document.getElementsByName('category') as NodeListOf<HTMLInputElement>;
         let checkdCategoryId = -1;
 
@@ -82,13 +76,17 @@ function Posting() {
     }
 
     const convertBase64 = (file: FileList | null, files: string[]) => {
-        if (file && file.length > 0) {
-            const fileReader = new FileReader();
-            fileReader.readAsDataURL(file[0]);
-            fileReader.onloadend = () => {
-                files.push(fileReader.result as string);
-            }
-        }
+        return new Promise<void>((resolve) => {
+            if (file && file.length > 0) {
+                const fileReader = new FileReader();
+                fileReader.readAsDataURL(file[0]);
+                fileReader.onloadend = () => {
+                    files.push(fileReader.result as string);
+                    resolve();
+                }
+            } else
+                resolve();
+        });
     }
 
     const getImageFiles = async () => {
@@ -97,10 +95,10 @@ function Posting() {
         const file3 = (document.getElementById('file3') as HTMLInputElement).files;
         let base64Files: string[] = [];
 
-        convertBase64(file1, base64Files);
-        convertBase64(file2, base64Files);
-        convertBase64(file3, base64Files);
-        
+        await convertBase64(file1, base64Files);
+        await convertBase64(file2, base64Files);
+        await convertBase64(file3, base64Files);
+
         return base64Files;
     }
 
@@ -108,35 +106,37 @@ function Posting() {
         event.preventDefault();
         const docTitle = title.trim();
         const description = document.getElementById('text-area') as HTMLTextAreaElement;
-        const goalInput = document.getElementById('goal') as HTMLInputElement;
-        const checkedCategoryId = await getCheckedCategory();
+        const goalInput = Number((document.getElementById('goal') as HTMLInputElement).value);
+        const checkedCategoryId = getCheckedCategory();
         const base64Files = await getImageFiles();
-   
+
         if (docTitle.length === 0 || docTitle === 'New Post Title')
             Swal.fire('글 제목을 입력해주세요.')
         else if (checkedCategoryId === -1)
             Swal.fire('카테고리를 선택해주세요.');
-        else if (goalInput.value === '')
-            Swal.fire('목표치를 입력해주세요.');
+        else if (goalInput > 1000 || goalInput< 1)
+            Swal.fire('목표치는 1 ~ 1000 범위 숫자여야 합니다.');
         else if (description.value.length < 10)
             Swal.fire('설명글을 10자 이상 적어주세요.');
         else if (base64Files.length === 0)
             Swal.fire('사진을 최소 1장 등록해주세요.');
-
-        // customAxios().post('/document', {
-        //     title: title,
-        //     context: description.value,
-        //     categoryId: selectedCategory,
-        //     goal: goalInput.value,
-        //     image: base64Files
-        // })
+        else {
+            customAxios().post('/document', {
+                title: docTitle,
+                context: description.value,
+                categoryId: checkedCategoryId,
+                goal: goalInput,
+                image: base64Files
+            }).then(() => {console.log('OK!')})
+        }
     }
 
-    const Layout = (): JSX.Element => {
-        return (
+    return (
+        <div id={isDesktop ? "desktop" : "mobile"}>
+            <FixedTop/>
             <div id="posting">
                 <form id="post-form">
-                    <input id="title" type="text" value={title} onChange={handleTitle}/>
+                    <input id="title" type="text" value={title} onChange={(e) => {setTitle(e.target.value)}}/>
                     <div id="category-wrapper">
                         <p>Category</p>
                         <div id="radio-list">
@@ -148,7 +148,7 @@ function Posting() {
                         </div>
                     </div>
                     <label>Goal<input id="goal" type="number" min="1" max="1000"/></label>
-                    <label>Description<textarea id="text-area"></textarea></label>
+                    <label>Description<textarea id="text-area" maxLength={500} onChange={(e) => setDescriptLength(e.target.value.length)}></textarea><div id="text-length">{descriptLength} / 500</div></label>
                     <div id="image-wrapper">
                         <p>Images</p>
                         <div id="file-list">
@@ -178,14 +178,7 @@ function Posting() {
                     <button type="submit" onClick={submitPost}>Post</button>
                 </form>
             </div>
-        )
-    }
-
-    return (
-        <>
-            {isMobile && <div id="mobile"><FixedTop/><Layout/></div>}
-            {isDesktop && <div id="desktop"><FixedTop/><Layout/></div>}
-        </>
+        </div>
     )
 }
 
