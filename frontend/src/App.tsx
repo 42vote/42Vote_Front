dcb945f0333a36016d1d3996104511a2a8d0c2f0
zoovely <yon041128@naver.com>
2023-06-06 17:31:28 +0900

    [FIX] 중복 posting route 삭제

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route, useLocation } from "react-router-dom";
-import "./App.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Detail from "./Detail/Detail";
 import MainIndex from "./Main/index";
 import NotFound from "./Etc/NotFound";
@@ -8,7 +8,7 @@ import Auth from "./Auth/page";
 import MyPage from "./Mypage";
 import Posting from "./Posting/Posting";
 import ProtectRoute from "./Auth/components/AuthProvider";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "./App.css";
 
 const queryClient = new QueryClient();
 
@@ -16,22 +16,19 @@ function App() {
   const location = useLocation();
   const locationPathName = location.pathname;
   const locationSearch = location.search;
+
   return (
     <QueryClientProvider client={queryClient}>
       <Routes location={location}>
         <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Login LoginText="Login with 42OAuth" />} />
-        <Route
-          path="/auth/42/redirect"
-          element={<Auth locationSearch={locationSearch} />}
-        />
+        <Route path="/auth/42/redirect" element={<Auth locationSearch={locationSearch} />} />
         <Route element={<ProtectRoute pathname={locationPathName} />}>
           <Route path="/main" element={<MainIndex />} />
           <Route path="/detail" element={<Detail />} />
           <Route path="/mypage" element={<MyPage />} />
           <Route path="/posting" element={<Posting />} />
           <Route path="/detail/*" element={<Detail />} />
-          <Route path="/posting" element={<Posting />} />
         </Route>
       </Routes>
     </QueryClientProvider>
