a3bde80337211a6326dd1ed040f4ac87c7529d28
parkjoungwan <whddhs6645@gmail.com>
2023-06-21 12:14:22 +0900

    [EDIT] tokenRefresh logic fixed

--- frontend/src/Auth/components/AuthProvider.tsx
+++ frontend/src/Auth/components/AuthProvider.tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import { customAxios } from "../../Lib/customAxios";
 import Cookies from "js-cookie";
 import { onRefreshToken } from "../apis/authApi";
+import { isToken } from "typescript";
 
 interface ProtectRouteProps {
   pathname: string;
@@ -15,44 +16,60 @@ const ProtectRoute = (props: ProtectRouteProps): React.ReactElement | null => {
   const [isAdmin, setIsAdmin] = useState(false);
   const [isTokenExpired, setIsTokenExpired] = useState(false);
   const accessTokenExpirationString = Cookies.get("token_expire");
-  const accessTokenExpiration = accessTokenExpirationString ? parseInt(accessTokenExpirationString) : 42424242;
+  const currentTime = new Date().getTime();
+  const accessTokenExpiration = accessTokenExpirationString
+    ? parseInt(accessTokenExpirationString)
+    : currentTime + 100000;
 
   let accessTokenTimer: NodeJS.Timeout;
 
   const startTokenExpirationTimer = () => {
+    const timeGap = accessTokenExpiration - currentTime;
+    const leftTime = timeGap > 0 ? timeGap : 100000;
     accessTokenTimer = setTimeout(() => {
       setIsTokenExpired(true);
-    }, accessTokenExpiration - 10000)
-  }
+    }, leftTime);
+  };
+
+  const checkTokenExpiration = () => {
+    if (accessTokenExpiration && currentTime > accessTokenExpiration) {
+      setIsTokenExpired(true);
+    } else {
+      setIsTokenExpired(false);
+      startTokenExpirationTimer();
+    }
+  };
 
   const handleTokenRefresh = async () => {
     clearTimeout(accessTokenTimer);
     onRefreshToken();
     setIsTokenExpired(false);
     startTokenExpirationTimer();
-  }
+  };
 
   useEffect(() => {
-    startTokenExpirationTimer();
-
+    checkTokenExpiration();
     return () => {
       clearTimeout(accessTokenTimer);
-    }
-  }, [accessTokenExpiration])
+    };
+  }, []);
 
-  useEffect(()=>{
-    if (isTokenExpired)
-      handleTokenRefresh()
-  }, [isTokenExpired])
+  useEffect(() => {
+    if (isTokenExpired) handleTokenRefresh();
+  }, [isTokenExpired]);
+
+  useEffect(() => {
+    customAxios()
+      .get("/user/me")
+      .then((res) => {
+        setIsAdmin(res.data.isAdmin);
+      });
+  }, []);
 
   if (!tokenExist()) {
     alert("다시 로그인 해주세요.");
     return <Navigate to="/" />;
   }
-
-  customAxios().get('/user/me').then((res) => {
-    setIsAdmin(res.data.isAdmin);
-  });
 
   return (
     <>
