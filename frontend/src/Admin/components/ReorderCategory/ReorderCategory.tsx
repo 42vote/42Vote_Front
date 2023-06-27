6bbc33ce436982746bc755bfad027e1fa3a5c879
박종완 <whddhs6645@gmail.com>
2023-06-27 13:50:59 +0900

    Update ReorderCategory.tsx
    임시저장

--- frontend/src/Admin/components/ReorderCategory/ReorderCategory.tsx
+++ frontend/src/Admin/components/ReorderCategory/ReorderCategory.tsx
@@ -1,22 +1,56 @@
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 import CategoryColumn from "./CategoryColumn";
-import { Column, Task, columnOrder, initialDataColumns, initialDataTasks } from "./initialData";
+import {
+  Column,
+  Task,
+  columnOrder,
+  initialDataColumns,
+  initialDataTasks,
+} from "./initialData";
 
 const ReorderCategory = () => {
-const TaskData: initialDataTasks = Task;
-const ColumnData: initialDataColumns = Column;
-const columnOrderData: string[] = columnOrder; 
+  const TaskData: initialDataTasks = Task;
+  const ColumnData: initialDataColumns = Column;
+  const columnOrderData: string[] = columnOrder;
   const onDragStart = () => {};
-  const onDragEnd = () => {};
+  const onDragEnd = (result) => {
+    const { destination, source, draggableId } = result;
+    if (!destination) {
+      return;
+    }
+    if (
+      destination.draggableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = ColumnData.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds,
+    }
+
+    const newData = {
+      columns: {
+        ...ColumnData.columns,
+        [newColumn.id]: newColumn,
+      }
+    }
+  };
 
   return (
     <DragDropContext onDragStart={onDragStart} onDragEnd={onDragEnd}>
-        {columnOrderData.map(columnId => {
-          const column = ColumnData.columns[columnId];
-          const tasks = column.taskIds.map(taskId => TaskData.tasks[taskId]);
+      {columnOrderData.map((columnId) => {
+        const column = ColumnData.columns[columnId];
+        const tasks = column.taskIds.map((taskId) => TaskData.tasks[taskId]);
 
-          return <CategoryColumn key={column.id} column={column} tasks={tasks} />;
-        })}
+        return <CategoryColumn key={column.id} column={column} tasks={tasks} />;
+      })}
     </DragDropContext>
   );
 };
