f6ee3cbe4259883c765d476697ce99cdcf9f4cb4
parkjoungwan <whddhs6645@gmail.com>
2023-07-02 22:25:58 +0900

    [EDIT] csv blob to a tag added at statisticsExport

--- frontend/src/Admin/components/Statistics/StatisticsExport.tsx
+++ frontend/src/Admin/components/Statistics/StatisticsExport.tsx
@@ -1,16 +1,33 @@
 import { ExportButton, ExportContainer } from "../../styles/styledComponents";
 import { useResponsive } from "../../../Main/customHooks/useResponsive";
 import { downloadCSV } from "../../apis/adminApis";
+import { useTags } from "../../../Main/customHooks/useTags";
 
 interface TagId {
-  tagId: string,
+  tagId: string;
 }
 //check is downloadCSV fixed in api server
 const StatisticsExport = (prop: TagId) => {
+  const tagInfo = useTags("all").data?.filter((arr) => arr.id === prop.tagId);
   const responsiveVar = useResponsive();
+  const downloadFd = () => {
+    downloadCSV(prop.tagId).then((res:Blob) => {
+      const blob = new Blob([res], {type: 'text/csv;charset=utf-8;'});
+      const fileUrl = window.URL.createObjectURL(blob);
+      const link = document.createElement('a');
+      link.href = fileUrl;
+      link.style.display = 'none';
+      link.download = tagInfo ? tagInfo[0].title + ".csv" : "csvFile.scv";
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+    });
+  };
   return (
     <ExportContainer responsiveVar={responsiveVar}>
-      <ExportButton onClick={ () => downloadCSV(prop.tagId)} isPatching={false}>통계 추출</ExportButton>
+      <ExportButton onClick={() => downloadFd()} isPatching={false}>
+        통계 추출 [.csv]
+      </ExportButton>
     </ExportContainer>
   );
 };
