e43ce35e6fdec71bc62fa7605e0f955320623468
zoovely <yon041128@naver.com>
2023-07-04 20:18:08 +0900

    [FIX] /vote/me param string to number

--- frontend/src/Detail/service/Update.tsx
+++ frontend/src/Detail/service/Update.tsx
@@ -1,11 +1,11 @@
 import { customAxios } from "../../Lib/customAxios";
 
 export const deleteVote = (docId: string) => {
-    return customAxios().delete('/vote/me', { data: { documentId: docId } });
+    return customAxios().delete('/vote/me', { data: { documentId: Number(docId) } });
 };
   
 export const postVote = (docId: string) => {
-    return customAxios().post('/vote/me', { documentId: docId });
+    return customAxios().post('/vote/me', { documentId: Number(docId) });
 };
 
 export const deleteDocument = (docId: string) => {
